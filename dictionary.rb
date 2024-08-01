ad4b2eadd46d37ef9fe56758f08ceef206e7767b
Booi853 <167473217+Booi853@users.noreply.github.com>
2024-08-01 17:11:21 +0800

    Initial commit

---
+++ dictionary.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# Implements a standard "talking" dictionary behavior.
+# @abstract
+# @author Kedar Mhaswade
+class Dictionary
+
+  def lookup(word)
+
+  end
+  def talk(word)
+
+  end
+
+end
